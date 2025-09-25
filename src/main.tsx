7b51dec7a716b952d1586b4c946ca844ff89e302
kaito9220 <kunifuda.22rd@gmail.com>
2025-09-26 00:12:01 +0900

    fix: main.tsxを書き換えました

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// BrowserRouter の代わりに HashRouter をインポート
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* <BrowserRouter> を <HashRouter> で囲む */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
